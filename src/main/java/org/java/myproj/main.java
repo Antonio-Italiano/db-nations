0c18fb2dd4cbad6c89c7e94060a4367bba1f123c
Antonio-Italiano <antoitalia221@gmail.com>
2023-05-22 15:37:45 +0200

    first commit

---
+++ src/main/java/org/java/myproj/main.java
@@ -0,0 +1,55 @@
+package org.java.myproj;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+			
+			String url = "jdbc:mysql://localhost:3306/Nations";
+			String user = "root";
+			String password = "Root";
+			
+			try (Connection con = DriverManager.getConnection(url, user, password)){
+					
+				Scanner sc = new Scanner(System.in);
+				System.out.println("Inserisci parametro di ricerca");
+				String param = sc.nextLine();
+			    sc.close();
+				String sql = "SELECT countries.name, countries.country_id, regions.name, continents.name "
+						+ "FROM countries "
+						+ "JOIN regions " + "ON countries.region_id = regions.region_id "
+						+ "JOIN continents " + "ON regions.continent_id = continents.continent_id "
+						+ "WHERE countries.name LIKE " + "'%" + param + "%'"
+						+ "ORDER BY countries.name;";
+				
+				try (PreparedStatement ps = con.prepareStatement(sql)) {
+					
+					try (ResultSet rs = ps.executeQuery()) {
+						
+						System.out.println("Nation - ID - Regions - Continents");
+						
+						while(rs.next()) {
+							
+							final String countryName = rs.getString(1);
+							final int countryId = rs.getInt(2);
+							final String regionName = rs.getString(3);
+							final String continentName = rs.getString(4);
+							System.out.println(countryName 
+									+ " - " + countryId 
+									+ " - " + regionName 
+									+ " - " + continentName);						}
+					}				
+				} catch (SQLException ex) {
+					System.err.println("Query not well formed");
+				}
+			} catch (SQLException ex) {
+				System.err.println("Error during connection to db");
+		}
+	}
+}
